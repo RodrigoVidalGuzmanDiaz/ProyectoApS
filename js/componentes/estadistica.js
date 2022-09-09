91f425148132ed8c8de7495ad9fd3e71fc420750
Rodrigo Vidal Guzmán Díaz <rvguzman@scl.edu.gt>
2022-09-08 18:33:23 -0600

    Se agrega mi parte del trabajo

---
+++ js/componentes/estadistica.js
@@ -0,0 +1,19 @@
+export  let estadistica1 =  
+    `<section id="contenido">
+        <div>
+            <div id="alineacion">
+                <div id="posicion">
+                    <div id="cajaVerde">
+                        Se recomienda reducir la ingesta de grasas saturadas por las poliinsaturadas y las monoinsaturadas que están presentes en el pescado y en los vegetales.
+                    </div>
+                </div>
+            </div>
+        
+        </div>
+        <div id="plato"><img src="https://github.com/RodrigoVidalGuzmanDiaz/IMAGES/blob/main/1x-copia/plato.png?raw=true"></div>
+        <div id="alineacion2"> 
+            <div id="cajaNaranja"> Las grasas no deben superar el 30% de las calorias totales ingeridas. Las proteinas deben de suponer el 15% restante en la dieta. Aunque parece fácil de seguir, la dieta de las sociedades más desarrolladas no cumplen estas proporciones y el aporte de grasas y proteínas es mucho mayor de lo que debiera.</div>
+            <div id="cajaVesh">Los hidratos de carbono deben aportar al menos un 55-60% del aporte calórico total.</div>  </div>
+      
+    </section>
+    <div id="titulo">Estadística</div>`;
