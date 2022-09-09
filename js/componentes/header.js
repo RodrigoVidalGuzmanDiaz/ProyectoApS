f0ee9630f6a87adc4f094ebc59e38976df1aab5c
Rodrigo Vidal Guzmán Díaz <rvguzman@scl.edu.gt>
2022-09-08 18:27:28 -0600

    Se completa la parte superior del trabajo

---
+++ js/componentes/header.js
@@ -0,0 +1,10 @@
+export  let header1 = 
+    `<div id="imgLogo">
+            <img src="https://github.com/RodrigoVidalGuzmanDiaz/IMAGES/blob/main/1x-copia/recurso1DigitalColor.png?raw=true">
+        </div>
+        <div id="botones">
+            <div id="btOpciones">   <a href="#">Proteinas</a>      </div>
+            <div id="btOpciones">   <a href="#">Carbohidratos</a>  </div>
+            <div id="btOpciones">   <a href="#">Lípidos</a>        </div>
+            <div id="btOpciones">   <a href="#">Vitaminas</a>      </div>
+        </div>`;
---
+++ js/main.js
@@ -0,0 +1,22 @@
+let root = document.querySelector("#root");
+
+root.innerHTML = `    
+    <header id="header"></header>
+    <section id="estadistica"></section>
+    <section id="piramide"></section>`;
+
+//agregando el header al root
+import {header1} from './componentes/header.js';
+let header = document.querySelector("#header");
+header.innerHTML = header1;
+
+
+//agregando la estadistica al root
+import {estadistica1} from './componentes/estadistica.js';
+let estadistica = document.querySelector("#estadistica");
+estadistica.innerHTML = estadistica1;
+
+//agregando la piramide al root
+//import {piramide1} from './componentes/piramide.js';
+//let piramide = document.querySelector("#piramide");
+//piramide.innerHTML = piramide1;
