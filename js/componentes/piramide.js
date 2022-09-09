6bfca575b5eeb3530b8e73f1214a1e6411675671
Jrlopez <jrlopez@scl.edu.gt>
2022-09-08 23:26:12 -0600

    agregue parte de mi trabajo

---
+++ js/componentes/piramide.js
@@ -0,0 +1,15 @@
+export let piramide = `<div id="contenedorDos">
+<h1 id="tiPi">Pirámide alimenticia</h1>
+<img id="pira" src="https://github.com/jrlopez185/imagenes-suvir/blob/main/Piramide.png?raw=true">
+<p id="paV">En nutrición, la pirámide alimenticia es una <br>referencia a la proporción que nuestro cuerpo<br> necesita para mantener una dieta sana y <br>balanceada.Se recomienda la ingesta diaria de <br>55% de carbohidratos,30% de grasas y 15% de<br>proteínas,vitaminas,minerales y fibras</p>
+</div>
+<footer id="fo">
+<p id="nombreUno">Alisson Beatriz del Valle Chicoj</p>
+<p id="nombreUno">Jose Renato Lopez Pocon</p>
+<p id="nombreUno">Jorge Emanuel Aguilar Giron</p>
+<p id="nombreUno">Keyla Valesca Osorio Lemus</p>
+<p id="nombreUno">Maria Renee Garcia Ordoñes</p>
+<p id="nombreUno">Rodrigo Vidal Guzman Díaz</p>
+<p id="nombreUno">Franklin Sebastian Sandoval Hernandez</p>
+<p id="tete">Digital Healt</p>
+</footer>`;
