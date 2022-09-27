2dc1e2fa626d0d29f581bfa7b7fb3be85436c1aa
Emmanuel <jefuentes@scl.edu.gt>
2022-09-27 12:11:02 -0600

    data

---
+++ js/data/proteinas.js
@@ -0,0 +1,18 @@
+export let proteina = [
+
+    {
+        name: "Carne",
+        img : "https://static.dw.com/image/60116361_303.jpg" ,
+        descripcion: "",
+        proteina : 0.5,
+        carbohidratos : 0,
+        lipidos: 0.5
+    },
+    {
+
+    },
+    {},
+    {},
+    {}
+
+];
