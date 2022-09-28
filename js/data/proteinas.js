18ca2e7a7304cb7e320c56282776aa366e0a8a5a
franklin312 <85715903+franklin312@users.noreply.github.com>
2022-09-28 16:18:52 -0600

    agregado la información de las proteínas

--- js/data/proteinas.js
+++ js/data/proteinas.js
@@ -1,18 +1,95 @@
 export let proteina = [
 
-    {
-        name: "Carne",
-        img : "https://static.dw.com/image/60116361_303.jpg" ,
-        descripcion: "",
-        proteina : 0.5,
-        carbohidratos : 0,
-        lipidos: 0.5
-    },
-    {
 
-    },
-    {},
-    {},
-    {}
+{
+    name: "Carne",
+    img : "https://github.com/RodrigoVidalGuzmanDiaz/IMAGES/blob/d71dbff9e49b6460a0400fa141d8e2c3f25dc6cf/Imagenes/image%204.png?raw=true" ,
+    descripcion: "",
+    proteina : 0.5,
+    carbohidratos : 0.5,
+    lipidos: 0.2,
+},
 
-];
+{
+    name: "Aceitunas",
+    img : "https://raw.githubusercontent.com/RodrigoVidalGuzmanDiaz/IMAGES/d71dbff9e49b6460a0400fa141d8e2c3f25dc6cf/Imagenes/image%206.png" ,
+    descripcion: "",
+    proteina : 0.935,
+    carbohidratos : 0.0,
+    lipidos: 6.5,
+},
+{
+    name: "Fideos ",
+    img : "https://raw.githubusercontent.com/RodrigoVidalGuzmanDiaz/IMAGES/d71dbff9e49b6460a0400fa141d8e2c3f25dc6cf/Imagenes/image%2013.png" ,
+    descripcion: "",
+    proteina : 0.153,
+    carbohidratos : 0.831,
+    lipidos: 0.65,
+
+},
+{
+    name: "Piña",
+    img : "https://raw.githubusercontent.com/RodrigoVidalGuzmanDiaz/IMAGES/d71dbff9e49b6460a0400fa141d8e2c3f25dc6cf/Imagenes/image%2017.png" ,
+    descripcion: "",
+    proteina : 0,
+    carbohidratos : 0,
+    lipidos: 0,
+},
+
+{
+    name: "Aceite de coco",
+    img : "https://raw.githubusercontent.com/RodrigoVidalGuzmanDiaz/IMAGES/d71dbff9e49b6460a0400fa141d8e2c3f25dc6cf/Imagenes/image%2021.png" ,
+    descripcion: "",
+    proteina : 0,
+    carbohidratos : 0,
+    lipidos: 0.1,
+},
+{
+ name: "Salmón",
+    img : "https://github.com/RodrigoVidalGuzmanDiaz/IMAGES/blob/d71dbff9e49b6460a0400fa141d8e2c3f25dc6cf/Imagenes/image%205.png?raw=true" ,
+    descripcion: "",
+    proteina : 0.667,
+    carbohidratos : 0.0,
+    lipidos: 0.333,
+
+},
+{
+    name: "Aguacate",
+       img : "https://github.com/RodrigoVidalGuzmanDiaz/IMAGES/blob/d71dbff9e49b6460a0400fa141d8e2c3f25dc6cf/Imagenes/image%2011.png?raw=true" ,
+       descripcion: "",
+       proteina : 0.667,
+       carbohidratos : 0.333,
+       lipidos: 0.6,
+   
+   },
+
+   {
+    name: "pan blanco",
+       img : "https://raw.githubusercontent.com/RodrigoVidalGuzmanDiaz/IMAGES/d71dbff9e49b6460a0400fa141d8e2c3f25dc6cf/Imagenes/image%2015.png" ,
+       descripcion: "",
+       proteina : 0.139,
+       carbohidratos : 0.806,
+       lipidos: 0.56,
+   
+   },
+   {
+    name: "zanahoria",
+       img : "https://github.com/RodrigoVidalGuzmanDiaz/IMAGES/blob/d71dbff9e49b6460a0400fa141d8e2c3f25dc6cf/Imagenes/image%2019.png?raw=true" ,
+       descripcion: "",
+       proteina : 0.143,
+       carbohidratos : 0.857,
+       lipidos: 0.0,
+   
+   },
+   {
+    name: "Matequilla",
+       img : "https://github.com/RodrigoVidalGuzmanDiaz/IMAGES/blob/d71dbff9e49b6460a0400fa141d8e2c3f25dc6cf/Imagenes/image%2023.png?raw=true" ,
+       descripcion: "",
+       proteina : 0.0,
+       carbohidratos : 0.0,
+       lipidos: 0.1,
+   
+   },
+
+
+]
