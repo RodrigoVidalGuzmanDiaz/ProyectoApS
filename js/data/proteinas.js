a06c57f7efe18a1856d996c142ff17923133f07f
franklin312 <85715903+franklin312@users.noreply.github.com>
2022-10-05 21:58:18 -0600

    agregue la proteínas

--- js/data/proteinas.js
+++ js/data/proteinas.js
@@ -1,29 +1,71 @@
 export let proteina = [
     {
         name: "Ternera",
-        img : "https://github.com/RodrigoVidalGuzmanDiaz/IMAGES/blob/main/Imagenes/image%204.png?raw=true" ,
+        img : "https://raw.githubusercontent.com/RodrigoVidalGuzmanDiaz/IMAGES/main/Imagenes/img/Proteinas/image%204.png" ,
         descripcion: "Ternera(100g)" +'<br>'+'"Proteinas:20%"' + '<br>'+ "Carbohidratos:0% "+'<br>'+ "Lípídos:20%",
-
-        descripcion: "Ternera(100g)" +'<br>'+"Proteinas:20%" + '<br>'+ "Carbohidratos:0% "+'<br>'+ "Lípídos:20%",
-        proteina : 0.5,
-        carbohidratos : 0,
-        lipidos: 0.5,
+        grafico: "https://raw.githubusercontent.com/RodrigoVidalGuzmanDiaz/IMAGES/main/Imagenes/img/Graficas/graficaProteinas/image%2010.png"
     },
     {
         name: "Salmón",
-        img : "https://github.com/RodrigoVidalGuzmanDiaz/IMAGES/blob/main/Imagenes/image%206.png?raw=true" ,
+        img : "https://raw.githubusercontent.com/RodrigoVidalGuzmanDiaz/IMAGES/main/Imagenes/img/Proteinas/image%205.png" ,
         descripcion: "Salmón(100g)" +'<br>'+"Proteinas:20% "+'<br>'+"Carbohidratos:0% "+'<br>'+"Lípídos:10%",
-        proteina : 0.065,
-        carbohidratos : 0.0,
-        lipidos: 0.935,
+        grafico:"https://raw.githubusercontent.com/RodrigoVidalGuzmanDiaz/IMAGES/main/Imagenes/img/Graficas/graficaProteinas/image%208.png",
+    },
+   {
+        name: "Ternera",
+        img : "https://raw.githubusercontent.com/RodrigoVidalGuzmanDiaz/IMAGES/main/Imagenes/img/Proteinas/image%2028.png" ,
+        descripcion: "Ternera(100g)" +'<br>' + "Proteinas:15.9%" +'<br>'+"Carbohidratos:8.2%" + '<br>'+"Lípídos:75.9",
+        grafico:"https://raw.githubusercontent.com/RodrigoVidalGuzmanDiaz/IMAGES/main/Imagenes/img/Graficas/graficaProteinas/image%2029.png"
     },
     {
-        name: "Aceitunas",
-        img : "https://github.com/RodrigoVidalGuzmanDiaz/IMAGES/blob/main/Imagenes/image%206.png?raw=true" ,
-        descripcion: "Aceituna(5g)"+'<br>'+ "Proteinas:1.4%" + '<br>'+ "Carbohidratos:0% "+'<br>'+ "Lípídos:20%",
-        proteina : 0.065,
-        carbohidratos : 0.0,
-        lipidos: 0.935,
+        name: "Almendra",
+        img : "https://raw.githubusercontent.com/RodrigoVidalGuzmanDiaz/IMAGES/main/Imagenes/img/Proteinas/image%2030.png" ,
+        descripcion: "Almendra(100g)"+'<br>'+"Proteinas:13.3%" +'<br>'+ "Carbohidratos:8.8%" +'<br>' + "Lípídos:77.9%,",
+        grafico:"https://raw.githubusercontent.com/RodrigoVidalGuzmanDiaz/IMAGES/main/Imagenes/img/Graficas/graficaProteinas/image%2031.png",
     },
-]
 
+    {
+        name: "nueces",
+        img : "https://raw.githubusercontent.com/RodrigoVidalGuzmanDiaz/IMAGES/main/Imagenes/img/Proteinas/image%2032.png" ,
+        descripcion:"nueces(100g)"+'<br>'+"Proteinas:16.9%"+'<br>'+"Carbohidratos:8.4%"+'<br>'+"Lípídos:74.7%" ,
+        grafico:"https://raw.githubusercontent.com/RodrigoVidalGuzmanDiaz/IMAGES/main/Imagenes/img/Graficas/graficaProteinas/image%2033.png",
+    },
+
+    {
+        
+        name: "Nuez de macadamia",
+        img : "https://raw.githubusercontent.com/RodrigoVidalGuzmanDiaz/IMAGES/main/Imagenes/img/Proteinas/image%2034.png" ,
+        descripcion:"Nuez de macadamia(100g)"+'<br>'+"Proteinas:47.7% "+'<br>'+"Carbohidratos:4.5%"+'<br>'+"Lípídos:47.7%",
+        grafico:"https://raw.githubusercontent.com/RodrigoVidalGuzmanDiaz/IMAGES/main/Imagenes/img/Graficas/graficaProteinas/image%2035.png",
+    },
+    {
+    name: "pistacho",
+    img : "https://raw.githubusercontent.com/RodrigoVidalGuzmanDiaz/IMAGES/main/Imagenes/img/Proteinas/image%2036.png" ,
+    descripcion:"pistacho(100g)"+'<br>'+"Proteinas:21.4%"+'<br>'+"Carbohidratos:8.3%"+'<br>'+"Lípídos:70.3%",
+    grafico:"https://raw.githubusercontent.com/RodrigoVidalGuzmanDiaz/IMAGES/main/Imagenes/img/Graficas/graficaProteinas/image%2037.png"
+  },
+
+  {
+    name: "garbanzo",
+    img : "https://raw.githubusercontent.com/RodrigoVidalGuzmanDiaz/IMAGES/main/Imagenes/img/Proteinas/image%2038.png" ,
+    descripcion:"garbanzo(100g)"+'<br>'+"Proteinas:49.5%"+'<br>'+"Carbohidratos:3.4%"+'<br>'+"Lípídos:47.1%",
+    grafico:"https://raw.githubusercontent.com/RodrigoVidalGuzmanDiaz/IMAGES/main/Imagenes/img/Graficas/graficaProteinas/image%2039.png"
+  },
+
+  {
+    name: "guisantes seco",
+    img : "https://raw.githubusercontent.com/RodrigoVidalGuzmanDiaz/IMAGES/main/Imagenes/img/Proteinas/image%2040.png" ,
+    descripcion:"guisantes secos(100g)"+'<br>'+"Proteinas:40.0%"+'<br>'+"Carbohidratos:59.7%"+'<br>'+"Lípídos:0.3%",
+    grafico:"https://raw.githubusercontent.com/RodrigoVidalGuzmanDiaz/IMAGES/main/Imagenes/img/Graficas/graficaProteinas/image%2041.png"
+  },
+  
+  {
+    name: "Soja",
+    img : "https://raw.githubusercontent.com/RodrigoVidalGuzmanDiaz/IMAGES/main/Imagenes/img/Proteinas/image%2042.png" ,
+    descripcion:"Soja(100g)"+'<br>'+"Proteinas:23.4%"+'<br>'+"Carbohidratos:6.2%"+'<br>'+"Lípídos:70.4%",
+    grafico:"https://raw.githubusercontent.com/RodrigoVidalGuzmanDiaz/IMAGES/main/Imagenes/img/Graficas/graficaProteinas/image%2043.png"
+  },
+  
+
+
+]
