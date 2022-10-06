7861830fa128902ed030e887c21c9941c1819dd2
franklin312 <85715903+franklin312@users.noreply.github.com>
2022-10-05 22:12:22 -0600

    Agregue la parte de los lipidos

---
+++ js/data/Lipidos.js
@@ -0,0 +1,66 @@
+export let proteina = [
+    {
+        name: "Aceituna",
+        img : "https://raw.githubusercontent.com/RodrigoVidalGuzmanDiaz/IMAGES/main/Imagenes/img/Lipidos/image%206.png",
+        descripcion: "Aceituna(5g)"+'<br>'+"Proteinas:1.4%"+'<br>'+"Carbohidratos:0%"+'<br>'+"Lípídos:20%" ,
+        grafico:"https://raw.githubusercontent.com/RodrigoVidalGuzmanDiaz/IMAGES/main/Imagenes/img/Graficas/graficaLipidos/image%209.png",
+    },
+    {
+        name: "Aguacate",
+        img : "https://raw.githubusercontent.com/RodrigoVidalGuzmanDiaz/IMAGES/main/Imagenes/img/Lipidos/image%2011.png",
+        descripcion: "Aguacate(120g)"+'<br>'+"Proteinas:2%"+'<br>'+"Carbohidratos:10%"+'<br>'+"Lípídos:18%" ,
+        grafico:"https://raw.githubusercontent.com/RodrigoVidalGuzmanDiaz/IMAGES/main/Imagenes/img/Graficas/graficaLipidos/image%2012.png",
+    },
+    {
+        name: "Aceite de coco",
+        img : "https://raw.githubusercontent.com/RodrigoVidalGuzmanDiaz/IMAGES/main/Imagenes/img/Lipidos/image%2021.png",
+        descripcion: "Aceite de coco(10g)"+'<br>'+"Proteinas:0%" +'<br>'+"Carbohidratos:0%"+'<br>'+"Lípídos:10%" ,
+        grafico:"https://raw.githubusercontent.com/RodrigoVidalGuzmanDiaz/IMAGES/main/Imagenes/img/Graficas/graficaLipidos/image%2022.png",
+    },
+    {
+        name: "Mantequilla",
+        img : "https://raw.githubusercontent.com/RodrigoVidalGuzmanDiaz/IMAGES/main/Imagenes/img/Lipidos/image%2023.png",
+        descripcion: "Mantequilla(30g)"+'<br>'+"Proteinas:0%"+'<br>'+"Carbohidratos:0%"+'<br>'+"Lípídos:24%" ,
+        grafico:"https://raw.githubusercontent.com/RodrigoVidalGuzmanDiaz/IMAGES/main/Imagenes/img/Graficas/graficaLipidos/image%2022.png",
+    },
+    {
+        name: "Margarina de  soya",
+        img : "https://raw.githubusercontent.com/RodrigoVidalGuzmanDiaz/IMAGES/main/Imagenes/img/Lipidos/image%2058.png",
+        descripcion: "Margarina de  soya(100g)"+'<br>'+"Proteinas:0%"+'<br>'+"Carbohidratos:0%"+'<br>'+"Lípídos:10%" ,
+        grafico:"https://raw.githubusercontent.com/RodrigoVidalGuzmanDiaz/IMAGES/main/Imagenes/img/Graficas/graficaLipidos/image%2059.png",
+    },
+    
+    {
+        name: "Espinaca",
+        img : "https://raw.githubusercontent.com/RodrigoVidalGuzmanDiaz/IMAGES/main/Imagenes/img/Lipidos/image%2060.png",
+        descripcion: "Espinaca(100g)"+'<br>'+"Proteinas:0%"+'<br>'+" Carbohidratos:0%"+'<br>'+"Lípídos:10%" ,
+        grafico:"https://raw.githubusercontent.com/RodrigoVidalGuzmanDiaz/IMAGES/main/Imagenes/img/Graficas/graficaLipidos/image%2061.png",
+    },
+    {
+        name: "perejil",
+        img : "https://raw.githubusercontent.com/RodrigoVidalGuzmanDiaz/IMAGES/main/Imagenes/img/Lipidos/image%2062.png",
+        descripcion: "perejil(100g)"+'<br>'+"Proteinas:0.1%"+'<br>'+"Carbohidratos:6.4%"+'<br>'+"Lípídos:93.5%" ,
+        grafico:"https://raw.githubusercontent.com/RodrigoVidalGuzmanDiaz/IMAGES/main/Imagenes/img/Graficas/graficaLipidos/image%209.png",
+    },
+
+    {
+        name: "Apio",
+        img : "https://raw.githubusercontent.com/RodrigoVidalGuzmanDiaz/IMAGES/main/Imagenes/img/Lipidos/image%2064.png",
+        descripcion: "Apio(100g)"+'<br>'+"Proteinas:0.8%"+'<br>'+"Carbohidratos:3.4%"+'<br>'+"Lípídos:95.8%" ,
+        grafico:"https://raw.githubusercontent.com/RodrigoVidalGuzmanDiaz/IMAGES/main/Imagenes/img/Graficas/graficaLipidos/image%2065.png",
+    },
+
+    {
+        name: "Acelgas",
+        img : "https://raw.githubusercontent.com/RodrigoVidalGuzmanDiaz/IMAGES/main/Imagenes/img/Lipidos/image%2066.png",
+        descripcion: "Acelgas(100g)"+'<br>'+"Proteinas:0.3%"+'<br>'+"Carbohidratos:10.3%"+'<br>'+"Lípídos:89.4%" ,
+        grafico:"https://raw.githubusercontent.com/RodrigoVidalGuzmanDiaz/IMAGES/main/Imagenes/img/Graficas/graficaLipidos/image%2067.png",
+    },
+
+    {
+        name: "Brócoli",
+        img : "https://raw.githubusercontent.com/RodrigoVidalGuzmanDiaz/IMAGES/main/Imagenes/img/Lipidos/image%2068.png",
+        descripcion: "Brócoli(100g)"+'<br>'+"Proteinas:0.8%"+'<br>'+"Carbohidratos:5.1%"+'<br>'+"Lípídos:94.1%" ,
+        grafico:"https://raw.githubusercontent.com/RodrigoVidalGuzmanDiaz/IMAGES/main/Imagenes/img/Graficas/graficaLipidos/image%2069.png",
+    },
+]
